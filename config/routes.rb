f5723241b62b1a5cfaffb3879882f995b4b7a927
goce-arsoski <goce_ars@yahoo.com>
2020-08-13 01:37:46 +0200

    Sessions implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'recipes#index'
 
-  resources :users
   resources :recipes
+
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
