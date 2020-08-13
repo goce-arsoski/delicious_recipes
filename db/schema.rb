a5a5442be6dc8b7583de58a5f81c03a0f9af2b53
goce-arsoski <goce_ars@yahoo.com>
2020-08-13 13:05:53 +0200

    Create migration add_index_to_users_username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_225303) do
+ActiveRecord::Schema.define(version: 2020_08_13_110351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_225303) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "ingredients", "recipes"
