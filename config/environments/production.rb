680139cca890969d58a1d5fc6156ed9ca038c8c8
goce-arsoski <goce_ars@yahoo.com>
2020-08-17 17:23:52 +0200

    Show Background on heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
